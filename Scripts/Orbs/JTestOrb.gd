de7d8054c9d386e194246d1a4b84af5f30bbbd6f
JaggerV2026 <jvicente@uwm.edu>
2024-11-24 17:06:06 -0600

    Added basic orb repositioning
    Orbs will slerp to a defined position. It doesn't reach the exact correct position, so this can be accounted for in whatever runs the simulation/creates threads. Only repositions orbs if the orb has a reason to reposition like the player moving them.

---
+++ Scripts/Orbs/JTestOrb.gd
@@ -0,0 +1,26 @@
+class_name Orb extends Node3D
+
+var animalName = "default"
+var orbVelocity = 2.0
+var goalLoc = Vector3(0.0,0.5,-1.0) #Possibly set this in a handler 
+var moved = true #Change this to true when the orb is moved or placed. Change to false after locking in
+@export var population = 100
+
+signal inPosition
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#It's not exactly reaching the goalLoc. Just fix this in the handler.
+	#Once position stops updating, send a signal that says it's done moving
+	if(moved):
+		var lastPos = global_position
+		global_position = global_position.slerp(goalLoc, 2*delta)
+		if(lastPos == global_position):
+			moved = false
+			inPosition.emit(animalName)
+	
+	
