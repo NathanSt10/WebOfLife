bd36f769d3f7e4be1239fc2f8e78179d792f1a34
Nathan Stout <143974156+NathanSt10@users.noreply.github.com>
2024-11-27 18:16:34 -0600

    Refactored code
    Known bugs: Switching to a different controllers while nothing is selected can cause a crash. Need to determine how to handle left and right handed selection.

--- Scripts/Contollers/flashlightTechnique.gd
+++ Scripts/Contollers/flashlightTechnique.gd
@@ -12,13 +12,14 @@ var right_controller_model = preload("res://Models/Quest2Controllers/OculusQuest
 
 var highlighted_collider: MeshInstance3D
 var selected: MeshInstance3D
+@onready var controllers = get_tree().get_nodes_in_group("controllers")
 
 
 func _ready() -> void:
 	print("Flashlight Technique Ready")
 
 
-func initialize(new_tracker: String):
+func initialize():
 	active_selection = null
 	active_selection_pos = null
 	prev_selection = null
@@ -34,61 +35,55 @@ func initialize(new_tracker: String):
 	right_controller_model_scene.position = Vector3(0,0,.035)
 	if get_parent().is_left: add_child(left_controller_model_scene)
 	else: add_child(right_controller_model_scene)
-	print("Flashlight reinitialized with tracker:", tracker)
+	print("Flashlight reinitialized")
 
 
 
 func _process(delta: float) -> void:
-	#if $"ShapeCast3D".is_colliding():
-		#selections_list = $"ShapeCast3D".collision_result
-		#var closest = $"ShapeCast3D".get_collider(abs(offset) % selections_list.size())
-		#if closest != null: closest = closest.get_parent()
-		#if active_selection == null or (active_selection != closest and closest != null):
-			#prev_selection = active_selection
-			#if prev_selection != null: prev_selection.scale = Vector3(1, 1, 1)
-			#active_selection = closest
-			#if active_selection != null: active_selection.scale = Vector3(1.1, 1.1, 1.1)
-	#elif active_selection != null:
-		#offset = 0
-		#active_selection.scale = Vector3(1,1,1)
-		#active_selection = null
-		
-		
-	if $"ShapeCast3D".is_colliding():
-		highlighted_collider = $"ShapeCast3D".get_collider(0).get_parent()
-		print("colliding with %s" % $"ShapeCast3D".get_collider(0).get_parent().name)
-		highlighted_collider.scale = Vector3(1.1, 1.1, 1.1)
-	elif highlighted_collider and !selected:
-		print("There was a collider but now there isnt")
-		highlighted_collider.scale = Vector3(1, 1, 1)
-		highlighted_collider = null
-		
-	if selected:
-		update_selection_position()
+	if controllers[0].get_child(0).name == "Flashlight":
+		if $"ShapeCast3D".is_colliding():
+			highlighted_collider = $"ShapeCast3D".get_collider(0).get_parent()
+			print("colliding with %s" % $"ShapeCast3D".get_collider(0).get_parent().name)
+			highlighted_collider.scale = Vector3(1.1, 1.1, 1.1)
+		elif highlighted_collider and !selected:
+			print("There was a collider but now there isnt")
+			highlighted_collider.scale = Vector3(1, 1, 1)
+			highlighted_collider = null
+			
+		if selected:
+			update_selection_position()
 
 
 func enable_selection():
 	print("enable selection")
-	#visible = true
-	#$"ShapeCast3D".enabled = true
-	get_tree().get_nodes_in_group("controllers")[0].visible = true
-	get_tree().get_nodes_in_group("controllers")[1].visible = true
-	get_tree().get_nodes_in_group("controllers")[0].get_child(0).find_child("ShapeCast3D").enabled = true
-	get_tree().get_nodes_in_group("controllers")[1].get_child(0).find_child("ShapeCast3D").enabled = true
+	# Enable shapecast and make controllers visible
+	var controllers = get_tree().get_nodes_in_group("controllers")
+	if controllers[0].get_child(0).name == "Flashlight":
+		print("%s controller is flashlight" % controllers[0])
+		controllers[0].get_child(0).find_child("ShapeCast3D").enabled = true
+		controllers[0].get_child(0).visible = true
+	if controllers[1].get_child(0).name == "Flashlight":
+		print("%s controller is flashlight" % controllers[1])
+		controllers[1].get_child(0).find_child("ShapeCast3D").enabled = true
+		controllers[1].get_child(0).visible = true
+	
 	selected.scale = Vector3(1, 1, 1)
 	selected.global_position = selected.get_meta("original_position")
 
 func disable_selection():
 	print("disable selection")
-	#visible = false
-	#$"ShapeCast3D".enabled = false
+	# Disable shapcecast and make controllers invisible
+	if controllers[0].get_child(0).name == "Flashlight":
+		print("%s controller is flashlight" % controllers[0])
+		controllers[0].get_child(0).find_child("ShapeCast3D").enabled = false
+		controllers[0].get_child(0).visible = false
+	if controllers[1].get_child(0).name == "Flashlight":
+		print("%s controller is flashlight" % controllers[1])
+		controllers[1].get_child(0).find_child("ShapeCast3D").enabled = false
+		controllers[1].get_child(0).visible = false
+	
+	print("setting scale to .5")
 	selected.scale = Vector3(0.5, 0.5, 0.5)
-	get_tree().get_nodes_in_group("controllers")[0].visible = false
-	get_tree().get_nodes_in_group("controllers")[1].visible = false
-	get_tree().get_nodes_in_group("controllers")[0].get_child(0).find_child("ShapeCast3D").enabled = false
-	get_tree().get_nodes_in_group("controllers")[1].get_child(0).find_child("ShapeCast3D").enabled = false
-	
-	
 
 
 func update_selection_position():
@@ -97,16 +92,6 @@ func update_selection_position():
 
 
 func _on_button_pressed(name: String) -> void:
-	#if name == "grip_click":
-		#if active_selection != null:
-			## Save the original position of the active selection
-			#active_selection_pos = active_selection.global_transform.origin
-			#
-			## Move the selected object to the controller's position
-			#active_selection.global_transform.origin = global_transform.origin
-			#active_selection.scale = Vector3(0.5, 0.5, 0.5)
-			#cur_selected = active_selection
-			
 	if name == "grip_click" and highlighted_collider:
 		print("grip clicked")
 		selected = highlighted_collider
@@ -115,16 +100,6 @@ func _on_button_pressed(name: String) -> void:
 
 
 func _on_button_released(name: String) -> void:
-	#if name == "grip_click" and active_selection_pos != null:
-		## Return the selected object to its original position
-		#cur_selected.global_transform.origin = active_selection_pos
-		#cur_selected.scale = Vector3(1, 1, 1)
-		#
-		## Reset variables
-		#active_selection_pos = null
-		#cur_selected = null
-		
-		
 	if name == "grip_click" and selected:
 		print("Grip released")
 		enable_selection()
