006f0e7cb041c2f5a1e47d00d51c126c5cf1f565
JaggerV2026 <jvicente@uwm.edu>
2024-12-08 18:04:55 -0600

    Trying something with buttons
    Buttons can detect areas, but they can't be detected themselves. This should prevent them from being grabbed and scaled, but they can be selected by holding the controller over them or holding the flashlight on them. I'm going to test it in a bit.

--- Scripts/GUIButton.gd
+++ Scripts/GUIButton.gd
@@ -1,16 +1,37 @@
 extends Node3D
 
 @onready var gui: Node3D = $"."
+@onready var button_highlight: MeshInstance3D = $Button1Highlight
+var buttonHighlighted = false
+var lockInput = false #Prevents signal from being sent without reselecting the button
+var timeElapsed = 0.0
+var toggleTime = 3.0
 signal buttonInput
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+#Selection input is mediocre at best. A queue could possibly be implemented to make it so toggling
+#time can persist across hands, but it's incredibly unneccessary right now
 func _process(delta: float) -> void:
-	pass
+	if(buttonHighlighted and !lockInput):
+		timeElapsed += delta
+		if(timeElapsed >= toggleTime):
+			buttonInput.emit(self)
+			lockInput = true
 	
-func toggleButton():
-	buttonInput.emit(self)
-	#Add some visual feedback. This will need to be different for each button I think
+	
+func _on_area_entered(area: Area3D):
+	if(!buttonHighlighted):
+		button_highlight.visible = true
+		timeElapsed = 0.0
+		buttonHighlighted = true
+	
+func _on_area_exited(area: Area3D):
+	if(buttonHighlighted):
+		button_highlight.visible = false
+		timeElapsed = 0.0
+		buttonHighlighted = false
+		lockInput = false
