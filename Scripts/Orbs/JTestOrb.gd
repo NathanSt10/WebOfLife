c4b4c40a5dc92c10fe25bd34ea5dbce36b01519d
JaggerV2026 <jvicente@uwm.edu>
2024-12-09 17:21:12 -0600

    Added scaling population
    Population changes based on the scale of the orb. When the scale is within a certain range, the population will be set to a specific value.
    Additionally, I think I got the VR stuff to work. I'm going to test it more later. I want to see if scaling is added.

--- Scripts/Orbs/JTestOrb.gd
+++ Scripts/Orbs/JTestOrb.gd
@@ -3,8 +3,9 @@ class_name Orb extends Node3D
 @export var animalName = "default"
 @export var goalLoc = Vector3(0.0,0.5,-1.0) #Possibly set this in a handler 
 var moved = true #Change this to true when the orb is moved or placed. Change to false after locking in
-@export var initialPopulation = 100
+var initialPopulation = 200
 var population
+var lastScale = 1.0 #To prevent constant updating. Could be handled with controllers I think
 
 signal inPosition
 # Called when the node enters the scene tree for the first time.
@@ -22,5 +23,22 @@ func _process(delta: float) -> void:
 		if(global_position.distance_to(goalLoc) < 0.001): #Update this by adding an area3d that emits the signal when entered.
 			moved = false
 			inPosition.emit(animalName, self)
+	#Scale handling
+	if(lastScale != global_scale):
+		#Scale should be the same for all values, so this is a work around
+		if(scale.x > 0.9 and scale.x < 1.1):
+			initialPopulation = 200
+		elif(scale.x > 1.1 and scale.x < 1.5):
+			initialPopulation = 300
+		elif(scale.x >= 1.5 and scale.x < 2.0):
+			initialPopulation = 500
+		elif(scale.x > 2.0):
+			initialPopulation = 700
+		elif(scale.x < 0.9 and scale.x > 0.5):
+			initialPopulation = 100
+		elif(scale.x > 0.0 and scale.x <= 0.5):
+			initialPopulation = 50
+	lastScale = global_scale
+	
 	
 	
