3b8fe5227a87ae2ad4314a724c7e515a7da06971
JaggerV2026 <jvicente@uwm.edu>
2024-12-12 15:31:45 -0600

    JMain working
    JMain scene runs simulation with GUI. GUI can be moved with white bar, which I should add to the instructions in the future. I'll be cleaning some things up as I go, but orb placement is a priority.

--- Scripts/Orbs/JTestOrb.gd
+++ Scripts/Orbs/JTestOrb.gd
@@ -9,6 +9,7 @@ var population
 var lastScale = 1.0 #To prevent constant updating. Could be handled with controllers I think
 
 signal inPosition
+signal orbMoved
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	population = initialPopulation
@@ -22,18 +23,21 @@ func _process(delta: float) -> void:
 		global_position = global_position.slerp(goalLoc, 2*delta)
 		if(global_position.distance_to(goalLoc) < 0.001): #Update this by adding an area3d that emits the signal when entered.
 			moved = false
-			lastPos = global_position
+			global_position = goalLoc
+			lastPos = goalLoc
 			inPosition.emit(animalName, self)
 	else:
-		moved = (lastPos == global_position)
+		moved = !(lastPos == global_position)
+		if(moved):
+			orbMoved.emit(animalName, self)
 		lastPos = global_position
 		
 	#Scale handling
-	if(lastScale != global_scale):
+	if(lastScale != scale.x):
 		#Scale should be the same for all values, so this is a work around
-		if(scale.x > 0.9 and scale.x < 1.1):
+		if(scale.x > 0.9 and scale.x < 1.2):
 			initialPopulation = 200
-		elif(scale.x > 1.1 and scale.x < 1.5):
+		elif(scale.x > 1.2 and scale.x < 1.5):
 			initialPopulation = 300
 		elif(scale.x >= 1.5 and scale.x < 2.0):
 			initialPopulation = 500
@@ -43,7 +47,7 @@ func _process(delta: float) -> void:
 			initialPopulation = 100
 		elif(scale.x > 0.0 and scale.x <= 0.5):
 			initialPopulation = 50
-	lastScale = global_scale
+	lastScale = scale.x
 	
 	
 	
